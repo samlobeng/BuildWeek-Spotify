d7f5fbe5eb67f172c1c5fae06f01f4e4c65e3c19
Samuel Obeng <61053627+samlobeng@users.noreply.github.com>
2021-05-14 01:40:44 +0200

    adds animation to switcher

--- script.js
+++ script.js
@@ -11,18 +11,22 @@ window.onload = function () {
             e.currentTarget.querySelector('input').setAttribute('checked',true)
             let pageName = e.currentTarget.querySelector('input').value
             $('iframe')[0].setAttribute('src', pageName )
+            $('.card-wrapper')[0].classList.add('collapsed')
         })
     }
     //attach event handler to show/hide
-
+    
     $('.show-page-list')[0].addEventListener('click', (e) => {
-            let btn = e.currentTarget
+        let btn = e.currentTarget
             if( !btn.getAttribute('active') ) {
                 btn.setAttribute("active", true)
                 $('.card-wrapper')[0].classList.add('collapsed')
+                document.querySelector('.glass').classList.add('animate__animated animate__slideInUp')
             }else{
-                btn.removeAttribute("active")
+                 btn.removeAttribute("active")
                 $('.card-wrapper')[0].classList.remove('collapsed')
+               
+
 
             }
 
