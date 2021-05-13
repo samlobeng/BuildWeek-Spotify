3eeb0060b4e0314cee2004f3ea4a4d222e8ba8c4
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-05-13 20:55:25 +0200

    feat: static page switcher element

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-window.onload = function(){
+window.onload = function () {
+    let selectionsArr = [...$('.selection-card')]
+    //attach event handlers to every selection card
 
-    $('[value$=".html"]')
+
+    for (let elem of selectionsArr) {
+        elem.addEventListener('click', (e) => {
+            let inputs = [...e.currentTarget.parentNode.querySelectorAll('input')]
+            console.log(inputs)
+            for (let inputElem of inputs) {
+                inputElem.removeAttribute('checked')
+            }
+            e.currentTarget.querySelector('input').setAttribute('checked',true)
+        })
+    }
 }
